8a77a5e69eee7430d2755c77cd959d2ea81e100c
userName <userEmail>
2025-02-10 14:58:47 +0800

    2025-02-10

---
+++ player.py
@@ -0,0 +1,173 @@
+import os
+import random
+import time
+import math
+import cv2
+import numpy
+import pyautogui
+from PIL import ImageGrab
+
+# 获取当前项目的根路径
+base_dir = os.getcwd()
+
+# 初始化图片字典
+def load_img_dictionary(task_name):
+    # 根据 task_name 和 base_dir 动态调整图片目录
+    img_path = os.path.join(base_dir, task_name)
+    img_dictionary = {}
+    # 确保目录存在
+    if not os.path.exists(img_path):
+        raise FileNotFoundError(f"Directory {img_path} not found")
+    file_list = os.listdir(img_path)
+    for file in file_list:
+        name = file.split('.')[0]
+        file_path = os.path.join(img_path, file)
+        content = [cv2.imread(file_path), name]
+        img_dictionary[name] = content
+    return img_dictionary
+
+# 截图并转化为截图多维数组
+def get_screen_nd_array(name='screen'):
+    screen_img = ImageGrab.grab()
+    if name != 'screen':
+        screen_img.save(f'{base_dir}\\screen\\{name}.jpg')
+    # 将截图从PIL图像转换为N-dimensional array多维数组，然后将颜色空间从RGB转换为BGR（OpenCV默认使用BGR）
+    screen_nd_array = cv2.cvtColor(numpy.array(screen_img), cv2.COLOR_RGB2BGR)
+    return screen_nd_array
+
+
+def mark(background, p1, p2):
+    cv2.rectangle(background, p1, p2, (0, 0, 255), 3)
+
+
+# 随机位置偏移，默认左右5个像素
+def get_position_with_offset(position, range=5):
+    x, y = position
+    x += random.randint(-range, range)
+    y += random.randint(-range, range)
+    return (x, y)
+
+
+def player_click_without_delay(position):
+    # 电脑原本鼠标位置
+    originPosition = pyautogui.position()
+    x, y = get_position_with_offset(position)
+    duration = random.uniform(0.01, 0.02)
+    pyautogui.moveTo(x, y, duration=duration)
+    pyautogui.click()
+    pyautogui.moveTo(*originPosition, duration=duration)
+
+
+class Player(object):
+    def __init__(self, task_name, accuracy=0.8):
+        self.taskName = task_name
+        super(Player, self).__init__()
+        self.accuracy = accuracy
+        self.wait_second = 3
+        self.img_dictionary = load_img_dictionary(task_name)
+        self.screen_nd_array = get_screen_nd_array()
+
+    # 在background大图片上定位target_name对应的小图片位置
+    # debug开启则会以图片形式显示查找结果
+    def locate(self, background, img_name, debug=0):
+        position_2d_array = []
+        target, c_name = self.img_dictionary[img_name]
+        h, w, _ = target.shape
+        result = cv2.matchTemplate(background, target, cv2.TM_CCOEFF_NORMED)
+        location = numpy.where(result >= self.accuracy)
+        distance = lambda a, b: ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) ** 0.5  # 计算两点距离
+        for y, x in zip(*location):
+            center = x + int(w / 2), y + int(h / 2)
+            if position_2d_array and distance(position_2d_array[-1], center) < 20:  # 忽略邻近重复的点
+                continue
+            else:
+                position_2d_array.append(center)
+                p2 = x + w, y + h
+                mark(background, (x, y), p2)
+
+        if debug:  # 在图上显示寻找的结果，调试时开启
+            cv2.imshow(f'result for {img_name}:', background)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+        res = len(position_2d_array)
+        msg = f'查找结果：{c_name} 匹配到 {res} 个位置，识别精确度：{self.accuracy}'
+        print(msg)
+        if position_2d_array:
+            position_2d = position_2d_array[0]
+            return position_2d
+        return None
+
+    def match(self, exist_img_name):
+        screen_nd_array = get_screen_nd_array()
+        position_2d = self.locate(screen_nd_array, exist_img_name)
+        if position_2d is not None:
+            return position_2d
+        else:
+            return None
+
+    def player_click(self, position):
+        # 电脑原本鼠标位置
+        originPosition = pyautogui.position()
+        x, y = get_position_with_offset(position)
+        duration = random.uniform(0.01, 0.02)
+        pyautogui.moveTo(x, y, duration=duration)
+        pyautogui.click()
+        pyautogui.moveTo(*originPosition, duration=duration)
+        time.sleep(self.wait_second)
+
+    def match_and_click_with_delay(self, img_name, wait_second):
+        matched_position_2d = self.match(img_name)
+        if matched_position_2d is not None:
+            player_click_without_delay(matched_position_2d)
+            time.sleep(wait_second)
+
+    def match_and_click_primary(self, img_name_list):
+        for img_name in img_name_list:
+            matched_position_2d = self.match(img_name)
+            if matched_position_2d is not None:
+                self.player_click(matched_position_2d)
+                break
+
+    def match_and_click_by_order(self, img_name_list):
+        for img_name in img_name_list:
+            matched_position_2d = self.match(img_name)
+            if matched_position_2d is not None:
+                self.player_click(matched_position_2d)
+
+    def match_with_shift(self, img_name_with_shift):
+        screen_nd_array = get_screen_nd_array()
+        img_name, direction, distance = img_name_with_shift
+        position_2d = self.locate(screen_nd_array, img_name)
+        if position_2d is not None:
+            position_2d_with_shift = (
+                position_2d[0] + distance * math.cos(math.radians(direction)),
+                position_2d[1] + distance * math.sin(math.radians(direction))
+            )
+            return position_2d_with_shift
+
+    def match_and_click_by_order_with_shift(self, img_name_list_with_shift):
+        for img_name_with_shift in img_name_list_with_shift:
+            matched_position_2d_with_shift = self.match_with_shift(img_name_with_shift)
+            if matched_position_2d_with_shift is not None:
+                self.player_click(matched_position_2d_with_shift)
+
+    def match_with_shift_twice(self, img_name_with_shift_twice):
+        screen_nd_array = get_screen_nd_array()
+        img_name, direction_1, distance_1, direction_2, distance_2,  = img_name_with_shift_twice
+        position_2d = self.locate(screen_nd_array, img_name)
+        if position_2d is not None:
+            position_2d_with_shift = (
+                position_2d[0] + distance_1 * math.cos(math.radians(direction_1)),
+                position_2d[1] + distance_1 * math.sin(math.radians(direction_1))
+            )
+            position_2d_with_shift_twice = (
+                position_2d_with_shift[0] + distance_2 * math.cos(math.radians(direction_2)),
+                position_2d_with_shift[1] + distance_2 * math.sin(math.radians(direction_2))
+            )
+            return position_2d_with_shift_twice
+
+    def match_and_click_by_order_with_shift_twice(self, img_name_list_with_shift_twice):
+        for img_name_with_shift_twice in img_name_list_with_shift_twice:
+            matched_position_2d_with_shift_twice = self.match_with_shift_twice(img_name_with_shift_twice)
+            if matched_position_2d_with_shift_twice is not None:
+                self.player_click(matched_position_2d_with_shift_twice)
---
+++ task.py
@@ -0,0 +1,308 @@
+import time
+from player import Player
+
+front_player = Player('front')
+
+
+# 防御前哨基地 一举歼灭 获取奖励
+def defence_base():
+    front_player.match_and_click_by_order_with_shift([
+        ('free_shop', 90, 100),
+        ('destroy', 0, 0),
+        ('destroy_start', 0, 0),
+        ('REWARD', 0, 0),
+        ('destroy_cancel', 0, 0),
+        ('gain_reward', 0, 0),
+        ('REWARD', 0, 0),
+        ('gain_reward_click', 0, 0)
+    ])
+
+
+# 友情点
+def friend_points():
+    front_player.match_and_click_by_order([
+        'friend', 'friend_give',
+        'friend_confirm',
+        'friend_close'])
+
+# 邮箱
+def mail():
+    front_player.match_and_click_by_order(['mail', 'mail_get_all', 'REWARD', 'mail_close'])
+
+# 模拟室
+def simulation_room():
+    player = Player('simulation')
+    front_player.match_and_click_by_order(['ark', 'simulation_room'])
+    player.match_and_click_by_order(['simulation_start_1', 'simulation_difficulty_5', 'simulation_zone_c'])
+    if player.match('simulation_finished') is None:
+        player.match_and_click_by_order(['simulation_start_2'])
+        player.match_and_click_by_order([
+            'simulation_fast',
+            'simulation_skip_all',
+            'simulation_battle_begin',
+        ])
+        while True:
+            time.sleep(5)
+            if player.match('simulation_battle_end') is not None:
+                player.match_and_click_with_delay('simulation_battle_end', 5)
+                break
+        # 模拟结束
+        player.match_and_click_by_order([
+            'simulation_end',
+            'simulation_end_confirm'
+        ])
+    else:
+        player.match_and_click_with_delay('simulation_close', 3)
+    # 返回主页
+    front_player.match_and_click_with_delay('HOME', 3)
+
+# 免费商店
+def free_shop():
+    player = Player('free_shop')
+    front_player.match_and_click_by_order(['free_shop'])
+    player.match_and_click_by_order_with_shift([
+        # 普通商店领取每日免费物品
+        ('free_shop_normal', 90, 150),
+        ('free_shop_normal_0_diamond_purchase', 0, 0),
+    ])
+    front_player.match_and_click_with_delay('REWARD', 2)
+
+    # 竞技场商店购买每日物品
+    # 熔炉
+    player.match_and_click_by_order_with_shift([
+        ('arena_shop_unselected', 0, 0),
+        ('arena_shop_selected', 0, 660)
+    ])
+    player.match_and_click_with_delay('arena_shop_purchase', 0.3)
+    if player.match('free_shop_insufficient_funds') is not None:
+        time.sleep(1)
+        player.match_and_click_by_order(['arena_shop_purchase_cancel'])
+    else:
+        front_player.match_and_click_with_delay('REWARD', 2)
+
+
+    # 位置1
+    player.match_and_click_by_order_with_shift([
+        ('arena_shop_unselected', 0, 0),
+        ('arena_shop_selected', 0, 110)
+    ])
+    player.match_and_click_with_delay('arena_shop_purchase', 0.3)
+    if player.match('free_shop_insufficient_funds') is not None:
+        time.sleep(1)
+        front_player.match_and_click_with_delay('arena_shop_purchase_cancel', 2)
+    else:
+        front_player.match_and_click_with_delay('REWARD', 2)
+
+    # 位置2
+    player.match_and_click_by_order_with_shift([
+        ('arena_shop_unselected', 0, 0),
+        ('arena_shop_selected', 0, 220)
+    ])
+    player.match_and_click_with_delay('arena_shop_purchase', 0.3)
+    if player.match('free_shop_insufficient_funds') is not None:
+        time.sleep(1)
+        front_player.match_and_click_with_delay('arena_shop_purchase_cancel', 2)
+    else:
+        front_player.match_and_click_with_delay('REWARD', 2)
+
+    # 位置3
+    player.match_and_click_by_order_with_shift([
+        ('arena_shop_unselected', 0, 0),
+        ('arena_shop_selected', 0, 330)
+    ])
+    player.match_and_click_with_delay('arena_shop_purchase', 0.3)
+    if player.match('free_shop_insufficient_funds') is not None:
+        time.sleep(1)
+        front_player.match_and_click_with_delay('arena_shop_purchase_cancel', 2)
+    else:
+        front_player.match_and_click_with_delay('REWARD', 2)
+
+    # 位置4
+    player.match_and_click_by_order_with_shift([
+        ('arena_shop_unselected', 0, 0),
+        ('arena_shop_selected', 0, 440)
+    ])
+    player.match_and_click_with_delay('arena_shop_purchase', 0.3)
+    if player.match('free_shop_insufficient_funds') is not None:
+        time.sleep(1)
+        front_player.match_and_click_with_delay('arena_shop_purchase_cancel', 2)
+    else:
+        front_player.match_and_click_with_delay('REWARD', 2)
+        front_player.match_and_click_with_delay('RETURN', 2)
+
+
+# 付费商店
+def paid_shop():
+    player = Player('paid_shop')
+    daily = False
+    weekly = False
+    monthly = False
+    front_player.match_and_click_with_delay('paid_shop_1', 2)
+    front_player.match_and_click_with_delay('paid_shop_2', 2)
+    while not (daily & weekly & monthly):
+        # if daily & weekly & monthly:
+        #     break
+        player.match_and_click_by_order([
+            # 消费年龄限制
+            'paid_shop_age_limit',
+            'paid_shop_age_limit_confirm',
+            # 评价通知
+            'paid_shop_info_close'])
+        player.match_and_click_by_order_with_shift([
+            # 日周月免费钻石
+            ('paid_shop_gift', 0, 0),
+            ('paid_shop_gift_shake', 0, 0)
+        ])
+        # 每日
+        player.match_and_click_by_order_with_shift([('paid_shop_beginner_special_support_selected', 0, 120), ('paid_shop_free_diamond', 0, 0)])
+        front_player.match_and_click_with_delay('REWARD', 2)
+
+        paid_shop_free_diamond_daily_sold_out = player.match('paid_shop_free_diamond_daily_sold_out')
+        if paid_shop_free_diamond_daily_sold_out:
+            daily = True
+        # 每周
+        player.match_and_click_by_order_with_shift([('paid_shop_beginner_special_support_unselected', 0, 240), ('paid_shop_free_diamond', 0, 0)])
+        front_player.match_and_click_with_delay('REWARD', 2)
+
+        paid_shop_free_diamond_weekly_sold_out = player.match('paid_shop_free_diamond_weekly_sold_out')
+        if paid_shop_free_diamond_weekly_sold_out:
+            weekly = True
+        # 每月
+        player.match_and_click_by_order_with_shift([('paid_shop_beginner_special_support_unselected', 0, 360), ('paid_shop_free_diamond', 0, 0)])
+        front_player.match_and_click_with_delay('REWARD', 2)
+
+        paid_shop_free_diamond_monthly_sold_out = player.match('paid_shop_free_diamond_monthly_sold_out')
+        if paid_shop_free_diamond_monthly_sold_out:
+            monthly = True
+    player.match_and_click_with_delay('paid_shop_return', 1)
+
+# 咨询
+def consult():
+    player = Player('nikke_consult')
+    # 对第一个nikke进行咨询
+    front_player.match_and_click_by_order(['lobby', 'nikke', 'nikke_consult'])
+    player.match_and_click_by_order_with_shift([('nikke_consult_all', 90, 120)])
+    while True:
+        # 循环结束条件 指挥官体力用尽 —— 快速咨询
+        if player.match('nikke_consult_quick'):
+            # 进行快速咨询
+            player.match_and_click_with_delay('nikke_consult_quick', 0.1)
+            # 检测指挥官体力用尽,结束咨询
+            if player.match('nikke_consult_commander_end') is not None:
+                break
+            # 检测妮姬体力用尽,翻页并跳过本轮循环
+            if player.match('nikke_consult_nikke_end') is not None:
+                player.match_and_click_by_order(['nikke_consult_next'])
+                continue
+            # 否则进入快速咨询
+            else:
+                time.sleep(2.5)
+                front_player.match_and_click_by_order(['CONFIRM'])
+                player.match_and_click_by_order(['nikke_consult_next_step'])
+
+        # 循环结束条件 指挥官体力用尽 ——普通咨询
+        else:
+            # 进行普通咨询
+            player.match_and_click_with_delay('nikke_consult_normal', 0.1)
+            # 检测指挥官体力用尽,结束咨询
+            if player.match('nikke_consult_commander_end') is not None:
+                break
+            # 检测妮姬体力用尽,翻页并跳过本轮循环
+            if player.match('nikke_consult_nikke_end') is not None:
+                player.match_and_click_by_order(['nikke_consult_next'])
+                continue
+            # 否则进入普通咨询
+            else:
+                # 进入咨询动画
+                while True:
+                    # 进行普通咨询
+                    player.match_and_click_with_delay('nikke_consult_normal', 0.1)
+                    # 检测指挥官体力用尽,结束咨询动画
+                    if player.match('nikke_consult_commander_end') is not None:
+                        break
+                    # 检测妮姬体力用尽,结束咨询动画
+                    if player.match('nikke_consult_nikke_end') is not None:
+                        break
+                    front_player.match_and_click_by_order(['CONFIRM'])
+                    player.match_and_click_by_order([
+                        'nikke_consult_normal_auto', 'nikke_consult_normal_option_1',
+                        'nikke_consult_normal_skip', 'nikke_consult_next_step'
+                    ])
+                # 结束咨询动画并翻页
+                player.match_and_click_by_order(['nikke_consult_next'])
+    # 咨询结束，返回主页
+    front_player.match_and_click_by_order(['HOME'])
+
+
+# 新人竞技场
+def arena():
+    player = Player('arena')
+    front_player.match_and_click_with_delay('ark', 4)
+    front_player.match_and_click_with_delay('arena', 4)
+    player.match_and_click_with_delay('arena_beginner', 4)
+    while True:
+        arena_beginner_logo = player.match('arena_beginner_logo')
+        arena_beginner_battle_free = player.match('arena_beginner_battle_free')
+        if arena_beginner_logo is not None:
+            if arena_beginner_battle_free is None:
+                front_player.match_and_click_by_order(['HOME'])
+                break
+        player.match_and_click_by_order_with_shift([
+            ('arena_beginner_battle_free', 90, 220),
+            ('arena_beginner_battle_enter', 0, 0),
+            ('arena_next', 270, 200)
+        ])
+
+# 远征
+def trip():
+    front_player.match_and_click_with_delay('base', 5)
+    front_player.match_and_click_with_delay('trip', 5)
+    if front_player.match('gain_all_reward') is not None:
+        front_player.match_and_click_by_order(['trip_gain_all_reward', 'REWARD'])
+    front_player.match_and_click_by_order(['trip_send_all', 'trip_send', 'trip_close'])
+
+def mission():
+    front_player.match_and_click_by_order(['mission', 'mission_get_all', 'mission_get_all', 'REWARD', 'mission_close'])
+
+# 日常
+def daily_task():
+
+    # 付费商店
+    paid_shop()
+    time.sleep(5)
+
+    # 新人竞技场
+    # arena()
+    time.sleep(5)
+
+    # 防御前哨基地 一举歼灭 获取奖励
+    defence_base()
+    time.sleep(5)
+
+    # 友情点
+    friend_points()
+    time.sleep(5)
+
+    # 邮箱
+    mail()
+    time.sleep(5)
+
+    # 免费商店
+    free_shop()
+    time.sleep(5)
+
+    # 咨询
+    # consult()
+    time.sleep(5)
+
+    # 模拟
+    simulation_room()
+    time.sleep(5)
+
+    # 远征
+    trip()
+    time.sleep(5)
+
+    # 每日任务
+    mission()
+
